0d96d45f26ffb441ad905f46b248c64e573bc18a
tdesorme <tdesorme@uncc.edu>
2018-10-11 02:14:47 +0000

    4.7 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,6 +18,21 @@ def new
   @movie = Movie.new
   # default: render 'new' template
 end 
+        
+ def create
+    #@movie = Movie.create!(params[:movie]) #old way
+    @movie = Movie.create!(movie_params)  # new way
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+    
+     def update
+    @movie = Movie.find params[:id]
+    #@movie.update_attributes!(params[:movie])  # old way
+    @movie.update_attributes!(movie_params)  # new way  
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
     
     
     # add below all other methods
@@ -26,6 +41,5 @@ private
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
-    
     
 end
